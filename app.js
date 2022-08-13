6a39a7896e2434953d3502bdaf9a1e37f83875d4
Jasmin Dedic <jdedic1999@gmail.com>
2022-08-13 14:28:32 +0200

    Created some functionality

--- app.js
+++ app.js
@@ -1,81 +1,149 @@
 "use strict";
 
-/*
 
-// Variables to use
-let computerChoice = ["Rock", "Paper", "Scissors"];
-let scoreComputer = 0;
-let scorePlayer = 0;
-let winnerLoser;
+// Getting rock, paper and scissor images into variables
+let rock = document.querySelector("#rock");
+let paper = document.querySelector("#paper");
+let scissors = document.querySelector("#scissors");
 
-// Create a function that will randomly return either "Rock, Paper, or Scissors".
-function computerPlay(){
-    
+// Options for computer
+let computerRock = document.querySelector("#rock");
+let computerPaper = document.querySelector("#paper");
+let computerScissors = document.querySelector("#scissors");
+
+// Computer and Player scoring input fields
+let playerScoring = document.querySelector("#playerScoring");
+let computerScoring = document.querySelector("#computerScoring");
+
+// Set scoring values to zero
+let playerCounter = 0;
+let computerCounter = 0;
+
+// Setting variable for draw
+let draw = document.querySelector(".draw");
+
+// Create array and put rock, paper and scissors into it
+let computerChoice = [];
+
+computerChoice[0] = computerRock;
+computerChoice[1] = computerPaper;
+computerChoice[2] = computerScissors;
+
+// Create function that randomly picks rock, paper or scissors for computer
+function computerPlay() {
     let randComputer = computerChoice[Math.floor(Math.random() * computerChoice.length)];
     return randComputer;
-    
-};
-
-
-
-
-
-// Create a function that plays a single round of Rock Paper Scissors
-function playRound(playerSelection, computerSelection){
-
-    if(playerSelection == "Rock" && computerSelection == "Rock" || 
-        playerSelection == "Paper" && computerSelection == "Paper" ||
-        playerSelection == "Scissors" && computerSelection == "Scissors"){
-
-            return "It's a draw!";
-        }else if(playerSelection == "Rock" && computerSelection == "Scissors" ||
-                playerSelection == "Paper" && computerSelection == "Rock" ||
-                playerSelection == "Scissors" && computerSelection == "Paper"){
-                    return "You Win!";
-                }else{
-                    return "You Lose!";
-                    
-                }
 }
 
+// Put random computer value into variable
+//let computerChoiceValue = computerPlay();
 
+//Create event listener for rock image
+rock.addEventListener("click", () => {
+    // Put random computer value into variable
+    let computerChoiceValue = computerPlay();
 
+    // Remove active class from draw field to make it invisible
+    draw.classList.remove("active");
 
-
-function game(){
-
-    for (let i = 0; i < 5; i++){
-    
-    // Fills the the two parameters in the playRound method with values
-    const playerSelection = prompt("What do you choose? Rock, Paper or Scissors?");
-    const computerSelection = computerPlay();
-    
-    // Call method to play only one round
-    const roundWinner = playRound(playerSelection, computerSelection);
-
-    // Compare the value of playround method to give the score either player or computer
-    if (roundWinner == "It's a draw!"){
-        //console.log("It' a Draw, score keeps the same!");
-    }else if (roundWinner == "You Win!"){
-        scorePlayer++;
-    }else if (roundWinner == "You Lose!"){
-        scoreComputer++;
-    }
+    // When player and computer have the same value
+    if (rock == computerChoiceValue) {
+        draw.classList.add("active");
+    }else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
     }
 
-    if (scorePlayer == 3 || scorePlayer == 4 || scorePlayer == 5){
-        console.log("You Win! Congratulation!");
-        console.log("Player score: " + scorePlayer + " Computer score: " + scoreComputer);
-    }else if(scorePlayer == scoreComputer){
-        console.log("It's a Draw, both are bad");
-        console.log("Player score: " + scorePlayer + " Computer score: " + scoreComputer);
-    }else{
-        console.log("You Lose, Computer is better!");
-        console.log("Player score: " + scorePlayer + " Computer score: " + scoreComputer);
+    if (computerChoiceValue == paper && playerCounter < 3 && computerCounter < 3){
+        computerCounter++;
+        computerScoring.innerHTML = computerCounter;
+    } else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
     }
-}
 
-game();
+    if (computerChoiceValue == scissors && playerCounter < 3 && computerCounter < 3){
+        playerCounter++;
+        playerScoring.innerHTML = playerCounter;
+    }else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+})
 
 
-*/
+//Create event listener for paper image
+paper.addEventListener("click", () => {
+    // Put random computer value into variable
+    let computerChoiceValue = computerPlay();
+
+    // Remove active class from draw field to make it invisible
+    draw.classList.remove("active");
+
+    // When player and computer have the same value
+    if (paper == computerChoiceValue) {
+        draw.classList.add("active");
+    }else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+
+    if (computerChoiceValue == rock && playerCounter < 3 && computerCounter < 3){
+        playerCounter++;
+        playerScoring.innerHTML = playerCounter;
+    } else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+
+    if (computerChoiceValue == scissors && playerCounter < 3 && computerCounter < 3){
+        computerCounter++;
+        computerScoring.innerHTML = computerCounter;
+    }else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+})
+
+
+//Create event listener for scissors image
+scissors.addEventListener("click", () => {
+    // Put random computer value into variable
+    let computerChoiceValue = computerPlay();
+
+    // Remove active class from draw field to make it invisible
+    draw.classList.remove("active");
+
+    // When player and computer have the same value
+    if (scissors == computerChoiceValue) {
+        draw.classList.add("active");
+    }else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+
+    if (computerChoiceValue == rock && playerCounter < 3 && computerCounter < 3){
+        computerCounter++;
+        computerScoring.innerHTML = computerCounter;
+    } else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+
+    if (computerChoiceValue == paper && playerCounter < 3 && computerCounter < 3){
+        playerCounter++;
+        playerScoring.innerHTML = playerCounter;
+    }else if(computerCounter == 3) {
+        alert("You Lost, try again!!");
+    }else if(playerCounter == 3){
+        alert("You win, congratulation!!");
+    }
+})
